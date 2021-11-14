b12742e3331820413daa623fe6e171a4daf0a032
7Liberal7 <andreytennis66@gmail.com>
2021-11-14 22:54:09 +0300

    12homework 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function() {
-  
+$(function () {
+  $('.discounts__list').slick({
+    arrows: true,
+    dots: false, 
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-left.svg" alt="стрелка лево"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-right.svg" alt="стрелка право"></button>',
+  });
 })
