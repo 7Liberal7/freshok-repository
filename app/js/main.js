9685037a7efd3b04e12a7fe1fd925117931b8a1a
7Liberal7 <andreytennis66@gmail.com>
2021-11-15 13:20:49 +0300

    -am homework 12

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function () {
   $('.discounts__list').slick({
     arrows: true,
-    dots: false, 
+    dots: false,
     prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-left.svg" alt="стрелка лево"></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-right.svg" alt="стрелка право"></button>',
   });
+
+  var mixer = mixitup('.gallery-list');
 })
